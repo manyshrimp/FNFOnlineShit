bd825fe853b3993ebb4c2187d91d19f48de687d3
Sync <cornbreadboy18@gmail.com>
2023-02-22 19:10:59 -0600

    fixed
    oops

--- mods/data/insomnia/script.lua
+++
@@ -1,3 +0,0 @@
-function onCountdownStarted()
-    setProperty('skipCountdown',true)
-end
--- mods/data/left-unchecked/left-unchecked.lua
+++
@@ -1,6 +0,0 @@
-function onCreatePost()
-	for i = 0, 3 do--and not middlescroll then
-		setPropertyFromGroup("playerStrums", i, "alpha", 0.7)
-		setPropertyFromGroup("opponentStrums", i, "x", 1337)	
-	end
-end
